7325666e182877a4f951a9aee4e2868098716e6c
Ajai <ajai.np@gmail.com>
2017-12-27 15:54:19 +1100

    fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,36 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import {DataService} from '../data.service';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
- 
+
 })
 export class HomeComponent implements OnInit {
 
-  itemCount: number = 0;
-  btnText: string = "Add an item";
-  goalText: string;
+  itemCount: Number = 4;
+  btnText: string = 'Add an Item';
+  goalText: string = 'My life goal 1';
   goals = [];
-  constructor(private _data:DataService) { }
+
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
-    
     this._data.goal.subscribe(res => this.goals = res);
-    this._data.changeGoal(this.goals);
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   addItem() {
     this.goals.push(this.goalText);
-    this.goalText = '';
+    this.goalText = "";
     this.itemCount = this.goals.length;
     this._data.changeGoal(this.goals);
+
   }
 
   removeItem(i) {
     this.goals.splice(i, 1);
     this._data.changeGoal(this.goals);
+
   }
 }
